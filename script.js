4800a75c65586c034a47daeb856b30eb799ad139
Bruna Ribeiro <brunaribeiro.ufv@gmail.com>
2021-03-25 21:24:32 -0300

    Adiciona projeto squad7

---
+++ script.js
@@ -0,0 +1,18 @@
+var operacao = prompt("Digite 1 para primeira semana, 2 para segunda semana, 3 para terceira semana, 4 para quarta semana, 5 para quinta semana e 6 para sexta")
+
+if ( operacao == 1) {
+  document.write("VocÊ está em : Lógica de programação I  : Os primeiros programas com Javascript e HTML")
+
+} else if (operacao == 2) {
+  document.write("Você está em : Lógica de programação II  : Pratique com desenhos, animações e um jogo")
+} else if (operacao == 3){
+  document.write("Você está em : HTML5 e CSS3 parte 1 : A primeira página da Web")
+} else if (operacao == 4){1
+  document.write("Você está em : HTML5 e CSS3 parte 2 : Posicionamento, listas e navegação")
+} else if (operacao == 5){
+  document.write("Você esta em : Javascript : Programando na linguagem da web")
+} else if (operacao == 6){
+  document.write("Você esta em : Projeto Oracle ONE Lógica de Programação")
+}else {
+  document.write("Opção Inválida!")
+}
